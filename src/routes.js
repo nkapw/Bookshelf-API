0a5dfd029cd31de7457383bf0d29053feb70a2fa
Imam A. Fahrezi <imam@pop-os.localdomain>
2023-10-04 15:03:43 +0700

    first commit. success but not implement query parameter

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addBookHandler, findAllBooksHandler, findBookByIdHandler, updateBookByIdHandler, deleteBookByIdHandler } = require("./handler")
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/',
+        handler: findAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: findBookByIdHandler
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: updateBookByIdHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBookByIdHandler,
+    },
+]
+
+module.exports = routes
